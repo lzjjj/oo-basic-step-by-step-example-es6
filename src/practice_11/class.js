9ae391b571c2da5c1837a99600be0644da4f0b43
louis <louis.z.j.li@oocl.com>
2018-07-09 22:33:56 +0800

    [louis] complete  pritice-11

--- src/practice_11/class.js
+++ src/practice_11/class.js
@@ -1,1 +1,60 @@
+class Class {
+    constructor(number) {
+        this.member = []
+        this.number = number
+        this.handlers = []
+    }
+    on(eventType,cb) {
+        let self = this;
+        if(!(eventType in self.handlers)){
+            self.handlers[eventType] = [];
+        }
+        self.handlers[eventType].push(cb);
+        return self;
+    }
+    emit(eventType) {
+        let self = this;
+        let argsArr = Array.prototype.slice.call(arguments,1);
+        for(let i = 0;i<self.handlers[eventType].length;i++){
+            self.handlers[eventType][i].apply(self,argsArr);
+        }
+        return self;
+    }
+    getDisplayName() {
+        return `Class ${this.number}`
+    }
+    assignLeader(student) {
+        if (student.klass === this) {
+            this.leader = student
+            if('assignLeaderListener' in this.handlers){
+                this.emit('assignLeaderListener',student)
+            }
+        } else {
+            console.log('It is not one of us.')
+        }
+    }
+    appendMember(student) {
+        this.member.push(student)
+        if('assignJoinListener' in this.handlers){
+            this.emit('assignJoinListener',student)
+        }
+        
+        student.klass = this
+        
+    }
+    registerAssignLeaderListener(teacher) {
+        this.on('assignLeaderListener',(student)=>{
+            console.log(`I am ${teacher.name}. I know ${student.name} become Leader of Class ${student.klass.number}.`)
+        })
+    }
+    registerJoinListener(teacher){
+        this.on('assignJoinListener',(student)=>{
+            console.log(`I am ${teacher.name}. I know ${student.name} has joined Class ${this.number}.`)
+        })
+    }
+}
+export default Class
+
+
+
 
--- src/practice_11/teacher.js
+++ src/practice_11/teacher.js
@@ -1,1 +1,20 @@
 
+import Person from './person'
+class Teacher extends Person {
+    constructor(id,name, age,klasses) {
+        super(id,name, age)
+        this.klasses = klasses
+    }
+    introduce() {
+        return super.introduce() + ` I am a Teacher. I teach ${this.klasses&&this.klasses.length>0 ? 'Class '+ this.klasses.map(item=>item.number).join(", ") :'No Class'}.`
+    }
+    
+}
+export default Teacher
+
+
+
+
+
+
+
